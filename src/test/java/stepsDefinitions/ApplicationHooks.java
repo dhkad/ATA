747eee8e18cfb57f79dffc762846122bc59e5ff6
dhkad <49337171+dhkad@users.noreply.github.com>
2023-02-07 18:21:14 +0100

    Update ApplicationHooks.java

--- src/test/java/stepsDefinitions/ApplicationHooks.java
+++ src/test/java/stepsDefinitions/ApplicationHooks.java
@@ -39,15 +39,6 @@ public class ApplicationHooks {
 		driver.quit();
 	}
 
-	@After(order = 1)
-	public void tearDown(Scenario scenario) {
-		if (scenario.isFailed()) {
-			// take screenshot:
-			String screenshotName = scenario.getName().replaceAll(" ", "_");
-			byte[] sourcePath = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-			scenario.attach(sourcePath, "image/png", screenshotName);
-
-		}
-	}
+	
 
 }
